cba9efe3e7b5bd5fc5612bde50c5a4057fed3566
Trayan Iliev <office@iproduct.org>
2022-01-21 16:55:14 +0200

    JPQL DTO demo finished

--- 01-intro-spring-hibernate/src/main/java/course/hibernate/spring/dao/UserDetailsDtoRepository.java
+++ 01-intro-spring-hibernate/src/main/java/course/hibernate/spring/dao/UserDetailsDtoRepository.java
@@ -3,14 +3,15 @@ package course.hibernate.spring.dao;
 import course.hibernate.spring.dto.UserDetailsDto;
 import course.hibernate.spring.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface UserDetailsDtoRepository extends JpaRepository<User, Long> {
 //    @EntityGraph(value = "User.detail", type = EntityGraph.EntityGraphType.LOAD)
-//    @Query("SELECT NEW  course.hibernate.spring.dto.UserDetailsDto(u.id, u.firstName, u.lastName, u.username, GROUP_CONCAT(r)) " +
-//            "FROM User u LEFT JOIN u.roles r GROUP BY u.id")
+    @Query("SELECT NEW  course.hibernate.spring.dto.UserDetailsDto(u.id, u.firstName, u.lastName, u.username, GROUP_CONCAT(r)) " +
+            "FROM User u LEFT JOIN u.roles r GROUP BY u.id")
     List<UserDetailsDto> findAllBy();
     Optional<User> findByUsername(String username);
 }
