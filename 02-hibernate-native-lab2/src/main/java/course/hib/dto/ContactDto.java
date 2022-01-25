5820ef86758131bdd193b8306901d3dda07a3f19
Trayan Iliev <office@iproduct.org>
2022-01-25 17:56:35 +0200

    spring-boot-devtools bootstarter should be removed otherwise very strange bug arises when using entity classes in Hybernate manual bootstrap

---
+++ 02-hibernate-native-lab2/src/main/java/course/hib/dto/ContactDto.java
@@ -0,0 +1,17 @@
+package course.hib.dto;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.net.URL;
+
+@Data
+public class ContactDto implements Serializable {
+    private final Integer id;
+    private final String nameFirstName;
+    private final String nameMiddleName;
+    private final String nameLastName;
+    private final String notes;
+    private final URL website;
+    private final boolean starred;
+}
