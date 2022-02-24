675d8e6c103435d20d027a006f34edef5052547e
Trayan Iliev <office@iproduct.org>
2022-02-24 14:03:08 +0200

    demo project added

---
+++ 06-spring-hibernate-api-lab/src/main/java/course/hibernate/spring/entity/Book.java
@@ -0,0 +1,63 @@
+package course.hibernate.spring.entity;
+
+import lombok.*;
+import org.aspectj.lang.annotation.RequiredTypes;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+import org.hibernate.annotations.LazyGroup;
+import org.hibernate.annotations.NaturalId;
+import org.hibernate.annotations.Parameter;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import static javax.persistence.FetchType.LAZY;
+import static org.hibernate.id.UUIDGenerator.UUID_GEN_STRATEGY_CLASS;
+
+@Entity
+@Table(name = "books")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+    @Id
+    private Long id;
+    private String title;
+    @ManyToOne
+    private Person author;
+    @NaturalId
+    private String isbn;
+
+    @Basic(fetch=LAZY)
+    private String summary;
+
+    @Lob
+    @Basic(fetch=LAZY)
+    @LazyGroup("lobs")
+    private Blob image;
+
+    @Lob
+    @Basic(fetch=LAZY)
+    @LazyGroup("lobs")
+    private String sampleContent;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Book)) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
